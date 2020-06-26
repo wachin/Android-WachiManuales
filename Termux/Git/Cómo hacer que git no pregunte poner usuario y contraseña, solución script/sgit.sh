8984413685c26942989aee3eefc14fd146d9ea57
wachin <wachin.id@gmail.com>
2020-06-26 09:05:16 -0500

    Guardar automáticamente el commit P10 vie jun 26 09:05:16 -05 2020

---
+++ Termux/Git/Cómo hacer que git no pregunte poner usuario y contraseña, solución script/sgit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### Proyecto 1 ###
+cd /storage/emulated/0/github/Cancionero-Cristiano-con-acordes
+git add .
+git commit -m "Commit P1 $(date)"
+git push origin master
+
+
+### Proyecto 2 ###
+cd /storage/emulated/0/github/linux-android
+git add .
+git commit -m "Commit P2 $(date)"
+git push origin master
+
+### Proyecto 3 ###
+cd /storage/emulated/0/github/Mi-Diario
+git add .
+git commit -m "Commit P3 $(date)"
+git push origin master
+
+
